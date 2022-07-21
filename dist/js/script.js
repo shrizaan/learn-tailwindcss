66c002811624fb26bbfbbdcae8eca7ccb80b2cdf
Shahrizan <shahrizan.learner@gmail.com>
2022-07-21 15:00:18 +0700

    completed navbar detail style

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,15 @@
-const utilities = {};
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
 
+// Hamburger
 const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("click", function () {
   const spanHamburger = hamburger.querySelectorAll("span");
